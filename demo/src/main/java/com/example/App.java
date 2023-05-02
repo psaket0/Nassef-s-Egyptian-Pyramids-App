fc1d880f441fb54a6b3ac224a1f48c1b1616d46e
Saket Pathak <99856959+psaket0@users.noreply.github.com>
2023-05-01 20:53:02 -0700

    test 7

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -3,48 +3,49 @@ package com.example;
 import java.util.*;
 
 public class App {
-    // Define the pharaohs and their hieroglyphics
-    static final Map<String, String> pharaohs = new HashMap<>();
-    static {
-        pharaohs.put("Khufu", "fb560de8bc19833811a71a4f1f200f3cbf4efc075a6252fac178eb1d21142f85");
-        pharaohs.put("Djoser", "b14c10a77eabf9562f8b24a6b11aaf9df9e56d3c3f3d1f82148e4817b0ce22e8");
-        // add more pharaohs as needed
-    }
     
-    // Define the pyramids and their contributors
-    static final Map<Integer, List<String>> pyramids = new HashMap<>();
-    static {
-        pyramids.put(1, Arrays.asList("Khufu", "Djoser"));
-        pyramids.put(2, Arrays.asList("Djoser"));
-        // add more pyramids as needed
-    }
-    
-    // Define the contributors and their contributions
-    static final Map<String, Integer> contributors = new HashMap<>();
-    static {
-        contributors.put("Khufu", 1793);
-        contributors.put("Djoser", 1362);
-        // add more contributors as needed
-    }
+    private static Map<String, List<String>> pyramids;
+    private static Map<String, Integer> goldContributions;
+    private static Set<String> requestedPyramids;
     
     public static void main(String[] args) {
+        pyramids = new HashMap<>();
+        goldContributions = new HashMap<>();
+        requestedPyramids = new HashSet<>();
+        
+        // populate pyramids map
+        pyramids.put("Pyramid 1", Arrays.asList("Pharaoh 1", "Pharaoh 2"));
+        pyramids.put("Pyramid 2", Arrays.asList("Pharaoh 2", "Pharaoh 3"));
+        pyramids.put("Pyramid 3", Arrays.asList("Pharaoh 1", "Pharaoh 3"));
+        
+        // populate gold contributions map
+        goldContributions.put("Pharaoh 1", 100);
+        goldContributions.put("Pharaoh 2", 200);
+        goldContributions.put("Pharaoh 3", 300);
+        
         Scanner scanner = new Scanner(System.in);
-        boolean quit = false;
-        while (!quit) {
-            System.out.println("Enter a command (1-5, q):");
-            System.out.println("1. List all the pharaohs");
-            System.out.println("2. Displays information for a specific Egyptian pharaoh by asking the user for a pyramid id number");
-            System.out.println("3. List all the pyramids and the contributors associated with them");
-            System.out.println("4. Displays information for specific pyramid information by asking the user for a pyramid id number");
-            System.out.println("5. Reports a list of requested pyramids without any duplicates");
-            System.out.println("q. Quits the application");
-            String input = scanner.nextLine().trim().toLowerCase();
-            switch (input) {
+        
+        while (true) {
+            System.out.println("............Nassef's Egyptian Pyramid App................");
+            System.out.println("Command       Description");
+            System.out.println("..............");
+            System.out.println("1             List all of the pyramids and the contributors associated with them");
+            System.out.println("2             Display information for a specific pyramid");
+            System.out.println("3             Display a list of requested pyramids");
+            System.out.println("4             Display information for a specific pyramid by id number");
+            System.out.println("5             Report a list of requested pyramids without any duplicates");
+            System.out.println("q             Quit the application");
+            System.out.println("..........................................................");
+            System.out.print("Enter a command: ");
+            
+            String command = scanner.nextLine();
+            
+            switch (command) {
                 case "1":
-                    listPharaohs();
+                    listPyramids();
                     break;
                 case "2":
-                    displayPharaohInfo(scanner);
+                    displayPyramidInfo(scanner);
                     break;
                 case "3":
                     listPyramids();
@@ -53,24 +54,48 @@ public class App {
                     displayPyramidInfo(scanner);
                     break;
                 case "5":
-                    reportRequestedPyramids(scanner);
+                    reportRequestedPyramidsWithoutDuplicates();
                     break;
                 case "q":
-                    quit = true;
-                    break;
+                    System.out.println("Quitting application...");
+                    System.exit(0);
                 default:
-                    System.out.println("Invalid command.");
+                    System.out.println("Invalid command. Please try again.");
+                    break;
             }
-            System.out.println();
         }
     }
     
-    // Command 1: List all the pharaohs
-    static void listPharaohs() {
-        System.out.println("Pharaohs:");
-        for (String pharaoh : pharaohs.keySet()) {
-            System.out.println(pharaoh);
+    private static void reportRequestedPyramidsWithoutDuplicates() {
+    }
+
+    private static void listPyramids() {
+        System.out.println("List of all pyramids and contributors:");
+        for (Map.Entry<String, List<String>> entry : pyramids.entrySet()) {
+            String pyramid = entry.getKey();
+            List<String> contributors = entry.getValue();
+            System.out.println(pyramid + ": " + contributors);
         }
     }
     
-    
+    private static void displayPyramidInfo(Scanner scanner) {
+        System.out.print("Enter the name of the pyramid you want information on: ");
+        String pyramid = scanner.nextLine();
+        
+        List<String> contributors = pyramids.get(pyramid);
+        if (contributors == null) {
+            System.out.println("Pyramid not found.");
+            return;
+        }
+        
+        System.out.println("Information for " + pyramid + ":");
+        int totalGold = 0;
+        for (String contributor : contributors) {
+            int gold = goldContributions.getOrDefault(contributor, 0);
+            totalGold += gold;
+            System.out.println(contributor + " contributed " + gold + " gold.");
+        }
+        System.out.println("Total gold contributed: " + totalGold);
+        requestedPyramids.add(pyramid);
+    }
+}
